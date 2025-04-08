b73bbb6e05ef590cf66858c64838698f837318b4
SupakornThongaerd <supakorn642@gmail.com>
2025-04-08 12:57:25 +0700

    setup auth

--- login-auth-template/app/login/page.tsx
+++ login-auth-template/app/login/page.tsx
@@ -1,23 +1,13 @@
-import { FacebookButton } from "@/components/buttons/facebookButton"
-import { GithubButton } from "@/components/buttons/githubButton"
-import { GoogleButton } from "@/components/buttons/googlebutton"
-import { Card } from "@/components/card/index"
+import { Card } from "@/components/card"
+import { CardAnimation } from "@/components/card/cardAnimation"
 
-export const SignIn = () => {
-    return (
+export default function Login() {
+  return (
+    <div className="h-screen flex justify-center items-center p-4">
         <Card>
-            <div className="flex flex-col justify-between h-full min-w-full p-12">
-                <h1 className="text-4xl font-semibold max-w-[20rem] mb-6">
-                Sign in with your account
-                </h1>
-        
-                <div className="flex flex-col gap-3">
-                <GithubButton />
-                <GoogleButton />
-                <FacebookButton />
-                </div>
-            </div>
+            <CardAnimation />
         </Card>
-      
+    </div>
+    
     )
-  }
+}
